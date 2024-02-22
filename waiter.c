9e8c3fe6611eb68dc08ea8f4a5d7fc27e4e5301f
adi-42 <rajaditya2704@gmail.com>
2024-02-22 23:02:07 +0530

    added waiter

---
+++ waiter.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define MAX_CUSTOMERS 5 // given in the problem statement maximum number of customers won't exceed 5
+#define MAX_TABLE 5     // table size
+#define READ_END 0
+#define WRITE_END 1
+#define MENU "menu.txt"
+#define MAX_ORDER 50
+
+int main(){
+
+}
