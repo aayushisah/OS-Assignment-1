ccc0777c9b1d7a4f1ac36dd1ea700fce3d7a34f3
adi-42 <rajaditya2704@gmail.com>
2024-02-25 17:53:35 +0530

    extraction works with waiter

--- waiter.c
+++ waiter.c
@@ -12,13 +12,19 @@
 #define WRITE_END 1
 #define MENU "menu.txt"
 #define MAX_ORDER 10
-// double* returnPriceArray();
+double* returnPriceArray();
+int itemCount;
 int main()
 {
 	int waiterID;
 	printf("Enter Waiter ID: ");
 	scanf("%d", &waiterID);
 	int shouldWeContinue = 0;
+
+	double* prices = returnPriceArray();
+	for(int i=0; i<itemCount; i++){
+		printf("%lf\n", prices[i]);
+	}
 
 	do
 	{
@@ -62,7 +68,7 @@ int main()
 					{
 						break;
 					}
-					if (shared_orders[i][j] < 1 || shared_orders[i][j] > 4)
+					if (shared_orders[i][j] < 1 || shared_orders[i][j] > itemCount)
 					{
 						printf("order invalid. \n");
 						shared_orders[0][0] = 0;
@@ -91,8 +97,7 @@ int main()
 		// check total bill and creating new shared memory to send total bill to manager
 		if (shared_orders[0][0] == 2)
 		{
-			int total_bill = 0;
-			int prices[4] = {30, 40, 25, 30};
+			double total_bill = 0;
 
 			for (int i = 1; i < numberOfCustomer + 1; i++)
 			{
@@ -104,7 +109,7 @@ int main()
 				}
 			}
 
-			printf("Bill Amount for Table X: %d INR\n", total_bill);
+			printf("Bill Amount for Table X: %.2f INR\n", total_bill);
 			shared_orders[0][3] = total_bill;
 
 			// Creating a shared-memory between Manager-Waiter
@@ -130,7 +135,7 @@ int main()
 
 			*table_bills[waiterID] = total_bill;
 			int amt = *table_bills[waiterID]; // testing
-			printf("manager received bill: %d from waiter %d/n", amt, waiterID);
+			printf("manager received bill: %d from waiter %d\n", amt, waiterID);
 
 			shmdt(table_bills);
 		}
@@ -148,21 +153,21 @@ int main()
 	} while (shouldWeContinue != -1);
 }
 
-/*double* returnPriceArray(){
+double* returnPriceArray(){
 FILE *file;
 char line[100];
-int itemCount = 0;
+itemCount = 0;
 
 file = fopen("menu.txt", "r");
 if (file == NULL) {
 	printf("Error opening file\n");
-	return 1;
+	return (double*) 1;
 }
 
 //basically this function reads the first one digit
 while (fgets(line, sizeof(line), file)) {
 	int itemNumber;
-	if ((sscanf(line, "%d.", &itemNumber) == 1) {
+	if (sscanf(line, "%d.", &itemNumber) == 1) {
 		itemCount++;
 	}
 }
@@ -190,4 +195,4 @@ rewind(file); // Reset file pointer to start
 
 fclose(file);
 return arr;
-} */
+}
